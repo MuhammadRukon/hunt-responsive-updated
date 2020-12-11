5cf3c2ed2a9078ea17e0c6c7ab45792f7e234dc6
MuhammadRukon <muhammad.rukon619@gmail.com>
2020-12-11 17:16:41 +0600

    response slider fix

--- js/script.js
+++ js/script.js
@@ -31,8 +31,8 @@ $('.slider').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -71,10 +71,10 @@ $('.slider').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
